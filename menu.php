92aac66cb79f2b2b3b04111b2c461b948e8ba089
christian-greenman <74841252+christian-greenman@users.noreply.github.com>
2021-01-30 12:44:35 -0800

    css/file structure
    updated to .php from .html, created master css file for all pages, some css styling changes, added menu

---
+++ menu.php
@@ -0,0 +1,13 @@
+<div id="menu">
+  <ul>
+  	<li>
+      <a class="home" href="/">Home</a>
+    </li>
+    <li>
+      <a class="about" href="/about/">About</a>
+    </li>
+    <li>
+      <a class="team" href="/team/">The Team</a>
+    </li>
+  </ul>
+</div>
