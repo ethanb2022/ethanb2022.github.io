5346263689f6b1b1eff4b064114277ed36db775a
christian-greenman <74841252+christian-greenman@users.noreply.github.com>
2021-01-30 14:34:49 -0800

    Add files via upload

--- about/index.php
+++ about/index.php
@@ -9,29 +9,7 @@
     />
     <link rel="stylesheet" href="../style.css">
     <title>Group 14 | About</title>
-    <style>
-      p.title {
-        text-align: center;
-        font-size: xx-large;
-        font-weight: bold;
-      }
-      p.university {
-        text-align: center;
-        font-size: x-large;
-        font-weight: normal;
-      }
-      p.people {
-        text-align: center;
-        font-size: x-large;
-        font-weight: normal;
-      }
-      a.about {
-        font-size: x-large;
-      }
-      a.team {
-        font-size: x-large;
-      }
-    </style>
+
   </head>
   <body>
     <?php include '../menu.php';?>
