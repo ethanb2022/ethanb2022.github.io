11490b28dd665c9a116136fbbd18b6355c90c9b3
christian-greenman <74841252+christian-greenman@users.noreply.github.com>
2021-01-30 12:50:40 -0800

    Create index.php

---
+++ team/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="about team 14 senior project"
+    />    
+    <link rel="stylesheet" href="../style.css">
+    <title>Group 14 | Team</title>
+  </head>
+  <body>
+    <?php include '../menu.php';?>
+    <div class="container">
+      <p class="title">Team</p>
+      <div id="team-flex">
+        <div>
+          <img src="../images/person.png" alt="image of team member" />
+          <p class="title">Shawn Ray</p>
+          <p class="text">Description</p>
+        </div>
+        <div>
+          <img src="../images/person.png" alt="image of team member" />
+          <p class="title">Ethan Brown</p>
+          <p class="text">Description</p>
+        </div>
+        <div>
+          <img src="../images/person.png" alt="image of team member" />
+          <p class="title">Christian Greenman</p>
+          <p class="text">Description</p>
+        </div>
+        <div>
+          <img src="../images/person.png" alt="image of team member" />
+          <p class="title">Miguel Hernandez</p>
+          <p class="text">Description</p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
