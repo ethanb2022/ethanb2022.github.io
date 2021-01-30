c0f472ab413e695ac940e4a7be5b3411506c405d
christian-greenman <74841252+christian-greenman@users.noreply.github.com>
2021-01-30 12:50:14 -0800

    Create index.php

---
+++ about/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="about team 14 senior project"
+    />
+    <link rel="stylesheet" href="../style.css">
+    <title>Group 14 | About</title>
+    <style>
+      p.title {
+        text-align: center;
+        font-size: xx-large;
+        font-weight: bold;
+      }
+      p.university {
+        text-align: center;
+        font-size: x-large;
+        font-weight: normal;
+      }
+      p.people {
+        text-align: center;
+        font-size: x-large;
+        font-weight: normal;
+      }
+      a.about {
+        font-size: x-large;
+      }
+      a.team {
+        font-size: x-large;
+      }
+    </style>
+  </head>
+  <body>
+    <?php include '../menu.php';?>
+    <div class="container">
+      <p class="title">About</p>
+    </div>
+  </body>
+</html>
