0d12d2ec76b4423627b296969d0a30a18fd5b6a2
christian-greenman <74841252+christian-greenman@users.noreply.github.com>
2021-01-30 14:27:26 -0800

    Add files via upload

--- about/index.php
+++ about/index.php
@@ -37,6 +37,9 @@
     <?php include '../menu.php';?>
     <div class="container">
       <p class="title">About</p>
+      <p>
+        The goal of this project is to create software that can be used by doctors and radiologists to aid in cancer diagnoses.  When using the software, doctors will be able to upload a DICOM image from their computer or other machine, and put that image through one of many binary cancer detection networks for the respective organ that the image corresponds to.  When the binary cancer detection network is finished, the software will display the image once again, and highlight potential points of interest that might be cancerous.  
+      </p>
     </div>
   </body>
 </html>
